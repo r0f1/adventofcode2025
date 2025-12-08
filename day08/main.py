99eebff242405096ee97a8fb8661b540705ef9ac
Florian Rohrer <semicolonator@gmail.com>
2025-12-08 16:40:25 +0100

    Day 8 improved

--- day08/main.py
+++ day08/main.py
@@ -2,6 +2,7 @@ from math import prod
 from operator import itemgetter
 
 import numpy as np
+from scipy.cluster.hierarchy import DisjointSet
 from scipy.spatial.distance import pdist, squareform
 
 with open("input.txt") as f:
@@ -10,26 +11,12 @@ with open("input.txt") as f:
 n_nodes = len(coords)
 distances = sorted(np.ndenumerate(squareform(pdist(coords))), key=itemgetter(1))[n_nodes::2]
 
-clustering = [{i} for i in range(n_nodes)]
+clustering = DisjointSet(list(range(n_nodes)))
 for idx, ((n1, n2), _) in enumerate(distances):
-    curr = []
-    s1 = None
-    s2 = None
-    for s in clustering:
-        if n1 in s or n2 in s:
-            if n1 in s:
-                s1 = s
-            if n2 in s:
-                s2 = s
-        else:
-            curr.append(s)
-    if s1 == s2:
-        curr.append(s1)
-    else:
-        curr.append(s1.union(s2))
+    clustering.merge(n1, n2)
     if idx == 1000:
-        print(prod(sorted([len(c) for c in clustering])[-3:]))
-    clustering = curr
-    if len(clustering) == 1:
+        print(prod(sorted([len(c) for c in clustering.subsets()])[-3:]))
+
+    if clustering.n_subsets == 1:
         print(coords[n1][0] * coords[n2][0])
         break
